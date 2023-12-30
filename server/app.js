cce0275237a28907fbbf791b215c68522851f4a8
Ryan Scott <ryanamscott52@gmail.com>
2023-12-29 18:37:35 -0600

    Adds week 8 class activities.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import pizzas from "./routers/pizzas";
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 const PORT = process.env.PORT || 4040;
 
@@ -78,6 +94,8 @@ app.get("/weather/:city", (request, response) => {
     city
   });
 });
+
+app.use("/pizzas", pizzas);
 
 app.listen(PORT, () => console.log("Listening on port 4040"));
 
